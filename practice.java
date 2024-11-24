cda744f2579856a317ea5dd7837d2903a078b2a3
Angel9899 <117482489+Angel9899@users.noreply.github.com>
2024-11-24 13:23:31 -0500

    Update practice.java

--- practice.java
+++ practice.java
@@ -2,9 +2,6 @@ public class practice{
     public static void main(String[] args){
         practice2(2, 5);
         
-
-
-
     }
     static void practice2(int Cream , int Cake){
         if (Cream > Cake) {
