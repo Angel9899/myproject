949329c210414dc87f0c5f3fbfd8e13f0eb06b8b
Angel9899 <117482489+Angel9899@users.noreply.github.com>
2024-11-24 13:22:28 -0500

    Create practice.java

---
+++ practice.java
@@ -0,0 +1,16 @@
+public class practice{
+    public static void main(String[] args){
+        practice2(2, 5);
+        
+
+
+
+    }
+    static void practice2(int Cream , int Cake){
+        if (Cream > Cake) {
+            System.out.println("food");
+        }
+        
+
+    }
+}
